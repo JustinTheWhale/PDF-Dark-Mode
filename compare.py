690164234813e229d3d42054f7457fe11fae25f4
unknown <sjlee1218@postech.ac.kr>
2021-03-01 11:09:42 +0900

    Clone and revised pdf dark mode codes

--- compare.py
+++ compare.py
@@ -4,7 +4,7 @@ from numba import jit, uint8
 
 @jit(nopython=True, cache=True, fastmath={'fast'})
 def speed(a):
-    grey = np.full((3), fill_value=100, dtype=np.uint8)
+    grey = np.full((3), fill_value=70, dtype=np.uint8)
     for i in range(len(a)):
         for j in range(len(a[0])):
             if np.sum(a[i, j]) == 0:
