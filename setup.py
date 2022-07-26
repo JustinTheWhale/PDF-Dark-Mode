12fe221ff6ddbfe6895ba9d5baf6361fe9f691a5
Justin <justinraymen@gmail.com>
2022-07-25 20:10:09 -0700

    Bumped numba version to 0.55.2. Added Setup.py and added tox testing for multiple python versions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PDF-Dark-Mode',
+      version='1.0.2',
+      description='Converts PDFs to have a grey background to be easier on the eyes',
+      author='JustinTheWhale',
+      author_email='justinraymen@gmail.com',
+      url='https://github.com/JustinTheWhale/PDF-Dark-Mode',
+      install_requires=['opencv-python', 'numpy', 'fpdf', 'numba',
+                'pdf2image', 'Pillow', 'PyPDF2']
+     )
